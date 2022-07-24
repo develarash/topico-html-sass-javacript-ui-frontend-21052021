81f27ed76e701228774be7979b5da557a7cd0ec9
arashRamy <develarash@gmail.com>
2022-07-25 00:18:26 +0430

    first c

---
+++ script/js.js
@@ -0,0 +1,36 @@
+
+let menuReport = document.querySelector(".report-icon");
+let reportSection = document.querySelector(".report-section");
+
+menuReport.onclick=function(){
+    reportSection.classList.toggle("rpt");
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// window.onclick = function(event) {
+//     if (event.target == menuReport) {
+//         if(menuReport.classList.contains("rgt")){
+//     reportSection.classList.remove("rpt");
+
+// }
+// else{
+//     alert(nabod)
+// }}
+// }
